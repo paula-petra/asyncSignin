2fc241bf03a7427d2eaccf893b4b484da2d292ec
Paula-Petra <paulapetrai@gmail.com>
2023-04-14 16:46:33 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+//define constants
+const username = document.querySelector('#username').value;
+const password = document.querySelector('#password').value;
+const submit = document.querySelector('#submit');
+
+//prevent button default
+submit.addEventListener('click', (e) => {
+    login();
+    e.preventDefault();
+})
+
+//async form validation
+const login = async(username,password) => {
+    if(!username || !password) throw 'Missing Credentials';
+    if(username === 'paula' && password === "petra6425") return 'Welcome!'
+    throw 'Incorrect Username or Password'
+}
+
+login(username,password)
+    .then(msg => {
+        console.log("LOGIN SUCCESSFUL.")
+        console.log("msg")
+    })
+    .catch(err => {
+        console.log("ERROR!")
+        console.log(err)
+    })
